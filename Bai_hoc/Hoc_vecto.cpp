783939949db45adddfc58d416042da84e6b6689a
Bangyugi <bangtranvan08@gmail.com>
2023-07-10 11:47:03 +0700

    ssss

--- Bai_hoc/Hoc_vecto.cpp
+++ Bai_hoc/Hoc_vecto.cpp
@@ -4,10 +4,8 @@ int cnt1[10000001], cnt2[10000001];
 typedef long long ll;
 const int MOD = 1e9 + 7;
 
-int main()
+void vectorCoBan(vector<int> v)
 {
-    int n;
-    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     for (int i = 0; i < v.size(); i++)
     {
         cout << v[i] << " ";
@@ -30,4 +28,34 @@ int main()
     {
         cout << x << " ";
     }
+}
+
+void timKiemPhanTu(vector<int> v)
+{
+    int target = 5;
+    // Tìm kiếm phần tử target trong vecto
+    bool found = binary_search(v.begin(), v.end(), target);
+    if (found)
+    {
+        cout << "FOUND\n";
+    }
+    else
+    {
+        cout << "NOT FOUND\n";
+    }
+}
+
+void timGiaTriLonNhat(vector<int> v)
+{
+    auto max_val = max_element(v.begin(), v.end());
+    cout << "\nGia tri lon nhat trong vector la: " << max_val;
+    cout << "\nVi tri lon nhat trong vector la: " << distance(v.begin(), max_val);
+}
+
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    vectorCoBan(v);
+    timKiemPhanTu(v);
+    timGiaTriLonNhat(v);
 }
