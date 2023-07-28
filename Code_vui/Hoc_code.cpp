1b9a86763a7f900140c383880655462d023eff2a
Bangyugi <bangtranvan08@gmail.com>
2023-07-28 08:41:14 +0700

    ....

--- Code_vui/Hoc_code.cpp
+++ Code_vui/Hoc_code.cpp
@@ -1,53 +1,31 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-bool isprime(int n)
-{
-    if (n < 2)
-        return 0;
-    if (n == 2 || n == 3)
-        return 1;
-    if (n % 2 == 0 || n % 3 == 0)
-        return 0;
-    for (int i = 5; i < sqrt(n); i += 6)
-    {
-        if (n % i == 0 || n % (i + 2) == 0)
-            return 0;
-    }
-    return 1;
-}
-
-int Lmao(int n)
-{
-    int cnt = 0;
-    while (n > 0)
-    {
-        if (isprime(n % 10))
-        {
-            cnt++;
-        }
-        n /= 10;
-    }
-    return cnt;
-}
-
-bool cmp(int a, int b)
-{
-    return Lmao(a) > Lmao(b);
-}
 
 int main()
 {
+    int t;
     int n;
-    cin >> n;
+    cin >> t;
     int a[n];
-    for (int i = 0; i < n; i++)
+    int b[n];
+    int ans[n];
+    while (t--)
     {
-        cin >> a[i];
-    }
-    stable_sort(a, a + n, cmp);
-    for (auto x : a)
-    {
-        cout << x << " ";
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin >> b[i];
+        }
+        int B[n];
+        B[0] = b[0];
+        for (int i = 1; i < n; i++)
+        {
+            B[i] = B[i - 1] + b[i];
+        }
+        
     }
 }
