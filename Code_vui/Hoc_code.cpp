700de8c0e961b3d00759a2468c1d1e7e6446bba9
Bangyugi <bangtranvan08@gmail.com>
2023-07-29 09:32:32 +0700

    sss

--- Code_vui/Hoc_code.cpp
+++ Code_vui/Hoc_code.cpp
@@ -1,31 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
+
+using li = long long;
 
 int main()
 {
-    int t;
+
     int n;
-    cin >> t;
-    int a[n];
-    int b[n];
-    int ans[n];
-    while (t--)
+    cin >> n;
+    vector<li> a(n), b(n);
+    for (auto &x : a)
+        cin >> x;
+    for (auto &x : b)
+        cin >> x;
+    vector<li> sum(n + 1);
+    for (int i = 0; i < n; ++i)
+        sum[i + 1] = sum[i] + b[i];
+    vector<li> cnt(n + 1), add(n + 1);
+    for (int i = 0; i < n; ++i)
     {
-        cin >> n;
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-        }
-        for (int i = 0; i < n; i++)
-        {
-            cin >> b[i];
-        }
-        int B[n];
-        B[0] = b[0];
-        for (int i = 1; i < n; i++)
-        {
-            B[i] = B[i - 1] + b[i];
-        }
-        
+        int j = upper_bound(sum.begin(), sum.end(), a[i] + sum[i]) - sum.begin() - 1;
+        cnt[i] += 1;
+        cnt[j] -= 1;
+        add[j] += a[i] - sum[j] + sum[i];
     }
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << b[i] << " ";
+    }
+    cout << endl;
+    for (auto x : sum)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+    for (auto x : cnt)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+    for (auto x : add)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < n; ++i)
+    {
+        cout << cnt[i] * b[i] + add[i] << ' ';
+        cnt[i + 1] += cnt[i];
+    }
+    cout << '\n';
 }
